c80f42c9e71c86b28c37ed074342b3331145c3d8
pauldubois98 <pauldubois98@gmail.com>
2023-07-07 23:05:49 +0200

    dropped delete pt action

--- script.js
+++ script.js
@@ -178,11 +178,6 @@ addListenerMulti(road_canvas, 'pointerdown mousedown', function(event) {
                 road[editing_point_index][0],
                 road[editing_point_index][1], 5, "#FFF0", "#F00");
         }
-        if(event.button == 2 && editing_point_index>=0
-            && editing_point_index!=road.length-2
-            && editing_point_index!=1 ){
-                road = road.slice(0,editing_point_index).concat(road.slice(editing_point_index+1));
-            }
         if(event.button == 2 && editing_point_index==-1){
             var i = 0;
             while(i<road.length && road[i][0]<=x){
